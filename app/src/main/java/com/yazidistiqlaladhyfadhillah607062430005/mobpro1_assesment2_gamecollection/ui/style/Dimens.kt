093c471fa6305d9a6f9ffcd5975809fe47bbe895
Y4zidd <istiqlalyazid12@gmail.com>
2026-05-10 00:00:23 +0700

    style: add UI styles and additional resources

---
+++ app/src/main/java/com/yazidistiqlaladhyfadhillah607062430005/mobpro1_assesment2_gamecollection/ui/style/Dimens.kt
@@ -0,0 +1,13 @@
+package com.yazidistiqlaladhyfadhillah607062430005.mobpro1_assesment2_gamecollection.ui.style
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val paddingSmall = 8.dp
+    val paddingMedium = 16.dp
+    val paddingLarge = 24.dp
+    
+    val cardElevation = 2.dp
+    val iconSizeMedium = 24.dp
+    val iconSizeLarge = 64.dp
+}
